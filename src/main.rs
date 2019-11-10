b0a793fe7c5e1635b9a58f545a5ba520cdd13825
chris ricketts <chris.ricketts@steribar.com>
2019-11-10 22:05:41 +0000

    set button pin as pull up, duh

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,17 @@ const APP: () = {
     struct Resources {
         gpiote: hal::target::GPIOTE,
         led1: Pin<Output<PushPull>>,
+        button1: Pin<Input<PullUp>>,
     }
 
     #[init]
     fn init(ctx: init::Context) -> init::LateResources {
         hprintln!("init started").unwrap();
+        let gpios = ctx.device.P0.split();
+
+        let led1 = gpios.p0_17.into_push_pull_output(Level::High).degrade();
+        let button1 = gpios.p0_13.into_pullup_input().degrade();
+
         // Configure GPIOTE Channel 0 to trigger an interrupt on any value change for button 1
         ctx.device
             .GPIOTE
@@ -33,19 +39,16 @@ const APP: () = {
         // Enable GPIOTE Channel 0 as interupt
         ctx.device.GPIOTE.intenset.write(|w| w.in0().set_bit());
 
-        let gpios = ctx.device.P0.split();
-
-        let led1 = gpios.p0_17.into_push_pull_output(Level::High).degrade();
-
         hprintln!("init done").unwrap();
 
         init::LateResources {
             gpiote: ctx.device.GPIOTE,
             led1,
+            button1,
         }
     }
 
-    #[task(binds = GPIOTE, resources = [gpiote, led1], priority = 99)]
+    #[task(binds = GPIOTE, resources = [gpiote, led1])]
     fn gpiote(ctx: gpiote::Context) {
         static mut LED1_STATE: bool = true;
         ctx.resources
@@ -54,7 +57,7 @@ const APP: () = {
             .iter()
             .nth(0)
             .unwrap()
-            .write(|w| unsafe { w.bits(0) });
+            .reset();
 
         hprintln!("Button 1 Interrupt - toggling LED 1").unwrap();
 
