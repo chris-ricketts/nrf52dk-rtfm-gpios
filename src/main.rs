6f6c136a9a8d8d6f2b1592564393fe6aae75dc32
chris ricketts <chris.ricketts@steribar.com>
2019-11-10 21:42:06 +0000

    clear gpiote event

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,23 @@
 
 extern crate panic_semihosting;
 
-use cortex_m_semihosting::hprintln;
-
 use nrf52832_hal as hal;
+
+use cortex_m_semihosting::hprintln;
+use hal::gpio::*;
+use hal::prelude::*;
 
 const BUTTON1: u8 = 13;
 
 #[rtfm::app(device = crate::hal::target, peripherals = true)]
 const APP: () = {
+    struct Resources {
+        gpiote: hal::target::GPIOTE,
+        led1: Pin<Output<PushPull>>,
+    }
+
     #[init]
-    fn init(ctx: init::Context) {
+    fn init(ctx: init::Context) -> init::LateResources {
         hprintln!("init started").unwrap();
         // Configure GPIOTE Channel 0 to trigger an interrupt on any value change for button 1
         ctx.device
@@ -21,25 +28,42 @@ const APP: () = {
             .iter()
             .nth(0)
             .unwrap()
-            .write(|w| unsafe { w.mode().event().polarity().toggle().psel().bits(BUTTON1) });
+            .write(|w| unsafe { w.mode().event().polarity().hi_to_lo().psel().bits(BUTTON1) });
 
         // Enable GPIOTE Channel 0 as interupt
         ctx.device.GPIOTE.intenset.write(|w| w.in0().set_bit());
 
+        let gpios = ctx.device.P0.split();
+
+        let led1 = gpios.p0_17.into_push_pull_output(Level::High).degrade();
+
         hprintln!("init done").unwrap();
+
+        init::LateResources {
+            gpiote: ctx.device.GPIOTE,
+            led1,
+        }
     }
 
-    #[idle]
-    fn idle(_: idle::Context) -> ! {
-        hprintln!("idle").unwrap();
-        loop {}
-    }
+    #[task(binds = GPIOTE, resources = [gpiote, led1], priority = 99)]
+    fn gpiote(ctx: gpiote::Context) {
+        static mut LED1_STATE: bool = true;
+        ctx.resources
+            .gpiote
+            .events_in
+            .iter()
+            .nth(0)
+            .unwrap()
+            .write(|w| unsafe { w.bits(0) });
 
-    // TODO Does not work properly - this interrupt in continuously triggered after the button is
-    // only pressed once
-    #[task(binds = GPIOTE)]
-    fn gpiote(_: gpiote::Context) {
-        hal::target::NVIC::unpend(hal::target::Interrupt::GPIOTE);
-        hprintln!("GPIO Interrupt Triggered").unwrap();
+        hprintln!("Button 1 Interrupt - toggling LED 1").unwrap();
+
+        if *LED1_STATE {
+            ctx.resources.led1.set_low();
+        } else {
+            ctx.resources.led1.set_high();
+        }
+
+        *LED1_STATE = !*LED1_STATE;
     }
 };
