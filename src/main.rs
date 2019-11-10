389b51b8ba68a08b276b49bdbca02564ab51b80d
chris ricketts <chris.ricketts@steribar.com>
2019-11-10 19:31:46 +0000

    try unpend

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@
 extern crate panic_semihosting;
 
 use cortex_m_semihosting::hprintln;
-use hal::gpio::p0::*;
-use hal::gpio::*;
 
 use nrf52832_hal as hal;
 
@@ -13,12 +11,8 @@ const BUTTON1: u8 = 13;
 
 #[rtfm::app(device = crate::hal::target, peripherals = true)]
 const APP: () = {
-    struct Resources {
-        gpiote: hal::target::GPIOTE,
-    }
-
     #[init]
-    fn init(ctx: init::Context) -> init::LateResources {
+    fn init(ctx: init::Context) {
         hprintln!("init started").unwrap();
         // Configure GPIOTE Channel 0 to trigger an interrupt on any value change for button 1
         ctx.device
@@ -33,9 +27,6 @@ const APP: () = {
         ctx.device.GPIOTE.intenset.write(|w| w.in0().set_bit());
 
         hprintln!("init done").unwrap();
-        init::LateResources {
-            gpiote: ctx.device.GPIOTE,
-        }
     }
 
     #[idle]
@@ -46,11 +37,9 @@ const APP: () = {
 
     // TODO Does not work properly - this interrupt in continuously triggered after the button is
     // only pressed once
-    #[task(binds = GPIOTE, resources = [gpiote])]
-    fn gpiote(ctx: gpiote::Context) {
-        // Tried disabling and renabling GPIOTE channel 0 event to no avail
-        ctx.resources.gpiote.intenclr.write(|w| w.in0().set_bit());
-        ctx.resources.gpiote.intenset.write(|w| w.in0().set_bit());
+    #[task(binds = GPIOTE)]
+    fn gpiote(_: gpiote::Context) {
+        cortex_m::peripheral::NVIC::unpend(hal::target::Interrupt::GPIOTE);
         hprintln!("GPIO Interrupt Triggered").unwrap();
     }
 };
